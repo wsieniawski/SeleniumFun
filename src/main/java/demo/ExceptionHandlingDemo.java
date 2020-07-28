0a5def4f40195370ff7727da708af8cdd76b13ae
wsieniawski <sieniawskidev@gmail.com>
2020-07-28 22:20:37 +0200

    added errors handling basics

---
+++ src/main/java/demo/ExceptionHandlingDemo.java
@@ -0,0 +1,21 @@
+package demo;
+
+public class ExceptionHandlingDemo {
+
+	public static void main(String[] args) {
+		try {
+			demo();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+	
+	public static void demo() throws Exception {
+		System.out.println("Hello World...!");
+		throw new ArithmeticException("not valid operation");
+		//int i = 1/0;
+		//System.out.println("Completed");
+	}
+
+}
